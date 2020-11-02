66b32cb54dc6fb6fc2cf87b992133748f543b7be
panch0us <godbryansk@yandex.ru>
2020-11-02 16:49:53 +0300

    Добавлен вариант ветвления, если выбран № 1 меню и выбрано 2 года

--- main_menu.py
+++ main_menu.py
@@ -1,15 +1,48 @@
+# Библотека для работы с датой
+import datetime
+# Импортируем собсвтенный модуль для работы с файлами
+import file_management
+# Библиотека для работы с документами .docx
+
+
 print('*** АНАЛИЗ ФАЙЛОВ СТАТИСТИКИ ИЦ ***\n\n'
       'Меню Программы:\n'
       '\t1. Tab46\n'
       '\t2. ...\n'
       '')
 
+
+# Выбираем пункт меню, можно только цифры
 user_selection = 0
 while user_selection < 1:
     try:
         user_selection = int(input('Введите номер файла для анализа: '))
     except ValueError:
-        print('Можно вводить только числа!')
+        print('Можно ввести только число!')
 
+# Выбираем количество лет для сравнения
+
+# Получаем текущий год
+current_year = datetime.datetime.now().year
+
+# Уточняем количество лет для анализа (минимум 2, максимум 5)
+count_year = 1
+while count_year < 2 or count_year > 5:
+    # Выполняется пока текущий год меньше 2 или больше 5
+    try:
+        count_year = int(input('Укажите количество лет для сравнения (доступно от 2 до 5)): '))
+    except ValueError:
+        print('Можно ввести только число!')
+
+# Если выбран № 1 в главном меню
 if user_selection == 1:
-    pass
+    # Если выбрано 2 года для анализа:
+    if count_year == 2:
+        print(f'\nВы выбрали № 1 в меню и 2 года для анализа\n'
+              f'Если текущий годе не {current_year}, то поменяйте насйтроки в ОС')
+
+        file_1 = file_management.File(name='Tab46.docx',
+                                      path='C:/Users/panchous/Desktop/home/data_python/2020/09_2020/ur')
+        file_2 = file_management.File(name='Tab46.docx',
+                                      path='C:/Users/panchous/Desktop/home/data_python/2019/09_2020/ur')
+        print(file_1, file_2)
