d823aa28c2f9ead8e075077505893154ab262097
panch0us <godbryansk@yandex.ru>
2020-11-02 19:51:56 +0300

    Добавлена возможность подключения файла через docx

--- file_management.py
+++ file_management.py
@@ -1,9 +1,14 @@
+# Библиотека для работы с документами .docx
+import docx
+
+
 class File:
     """Путь к файлу"""
 
     def __init__(self, name, path):
         self.name = name
         self.path = path
+        self.document = docx.Document(self.path + '\\' + self.name)
 
     def __repr__(self):
         return f"Имя файла:\t\t{self.name}\n" \
